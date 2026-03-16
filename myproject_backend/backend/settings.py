a86d8f28e7e4a91eb7b48432ce3d8f75c99da971
Alberto George <albertogeorge789@gmai.com>
2026-03-16 02:59:43 +0200

    fix static files storage

--- myproject_backend/backend/settings.py
+++ myproject_backend/backend/settings.py
@@ -454,7 +454,7 @@ USE_TZ        = True
 # ─── Static & Media ───────────────────────────────────────────────
 STATIC_URL          = '/static/'
 STATIC_ROOT         = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL           = '/media/'
 MEDIA_ROOT          = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD  = 'django.db.models.BigAutoField'
