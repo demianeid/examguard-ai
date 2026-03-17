fed6f2a497a6be11dfd222c9d9006aa5fdfdcfc9
Alberto George <albertogeorge789@gmai.com>
2026-03-18 00:41:45 +0200

    add new admin sandi

--- myproject_backend/create_super.py
+++ myproject_backend/create_super.py
@@ -1,28 +1,43 @@
 import os
 import django
 
-# إعداد بيئة دجانغو
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 django.setup()
 
 from authentication.models import BaseUser
 
 
-username = os.getenv("ADMIN_USERNAME")
-email = os.getenv("ADMIN_EMAIL")
-password = os.getenv("ADMIN_PASSWORD")
+admin_user = os.getenv("ADMIN_USERNAME")
+admin_email = os.getenv("ADMIN_EMAIL")
+admin_pass = os.getenv("ADMIN_PASSWORD")
 
-if all([username, email, password]):
-    if not BaseUser.objects.filter(is_superuser=True).exists():
+if all([admin_user, admin_email, admin_pass]):
+    if not BaseUser.objects.filter(username=admin_user).exists():
         BaseUser.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password,
+            username=admin_user,
+            email=admin_email,
+            password=admin_pass,
             first_name="Admin",
             last_name="Admin"
         )
-        print(f" Superuser '{username}' created successfully!")
+        print(f"✅ Main Admin '{admin_user}' created!")
     else:
-        print(" Superuser already exists!")
+        print(f"ℹ️ Main Admin '{admin_user}' already exists.")
 else:
-    print("⚠️ Skipping superuser creation: ADMIN_USERNAME, ADMIN_EMAIL, or ADMIN_PASSWORD not set in environment.")
+    print("⚠️ Skipping main admin: Variables not set in Railway.")
+
+my_user = os.getenv("new_user")
+my_email = os.getenv("new_email")
+my_pass = os.getenv("new_pass")
+
+if not BaseUser.objects.filter(username=my_user).exists():
+    BaseUser.objects.create_superuser(
+        username=my_user,
+        email=my_email,
+        password=my_pass,
+        first_name="Alberto",
+        last_name="George"
+    )
+    print(f"✅ Personal Admin '{my_user}' created!")
+else:
+    print(f"ℹ️ Personal Admin '{my_user}' already exists.")
