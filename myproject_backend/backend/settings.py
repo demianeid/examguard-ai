c7cc2eacfccede0e68b91d0a76f7cf657acdbe66
Alberto George <albertogeorge789@gmai.com>
2026-03-18 05:39:23 +0200

    add cloudinary storage

--- myproject_backend/backend/settings.py
+++ myproject_backend/backend/settings.py
@@ -488,11 +488,9 @@ DEFAULT_FROM_EMAIL  = config('DEFAULT_FROM_EMAIL', default='ExamGuard <noreply@e
 
 # ─── Cloudinary Storage ───────────────────────────────────────────
 INSTALLED_APPS += ['cloudinary_storage', 'cloudinary']
-
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
-
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
